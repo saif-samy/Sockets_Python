40f83b34611b77fe5788c125dda659f4dffc5ef7
Seifeldin Sami <saifsamy2001@gmail.com>
2023-04-14 21:46:43 +0200

    Add files via upload

---
+++ client.py
@@ -0,0 +1,61 @@
+import socket
+import sys
+import cv2
+import numpy as np
+import time
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+try: 
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('[STARTING] client is starting')
+except socket.error as err:
+    print("Socket creation failed with error: %s" % (err))
+    sys.exit()
+
+try:
+    client.connect(ADDR)
+except:
+    print('Error connecting to server!')
+    sys.exit()
+
+images_arr = []
+count = 0
+connected = True
+start_time = time.time()
+while connected:
+    msgLength = client.recv(64)
+    if msgLength:
+        msgLength = int(msgLength)
+        msg = client.recv(msgLength)
+        try:
+            if msg.decode(FORMAT) == '!DISCONNECT':
+                connected = False
+                print('[DISCONNECTED]')
+        except:
+            print('Packet Size:', msgLength, 'Packet Number:', count + 1)
+            count += 1
+            images_arr.append(msg)
+client.close()
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+img = images_arr[0]
+img = np.frombuffer(img, np.uint8)
+img = cv2.imdecode(img, 3)
+height, width, layers = img.shape
+size = (width,height)
+out = out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 24, size)
+
+for i in range(len(images_arr)):
+    img = images_arr[i]
+    img = np.frombuffer(img, np.uint8)
+    img = cv2.imdecode(img, 3)
+    out.write(img)
+out.release()
+        
---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+import sys
+import cv2
+
+HEADER = 64
+PORT = 5050
+# Getting the local address of the computer since we are running the server on our local network
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!DISCONNECT'
+
+try: 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('[STARTING] server is starting')
+except socket.error as err:
+    print("Socket creation failed with error: %s" % (err))
+    sys.exit()
+
+# Binding the socket with the local address of the computer
+server.bind(ADDR)
+
+server.listen()
+print(f'[LISTENING] server is listening on {SERVER}')
+
+def encodeImage(img):
+    return cv2.imencode('.jpeg', img)[1].tobytes()
+
+def send(message):
+    msgLength = len(message)
+    sendLength = str(msgLength).encode(FORMAT)
+    sendLength += b' ' * (HEADER - len(sendLength))
+    clientSocket.send(sendLength)
+    # This print has been added because two send functions directly after each other causes an error for some reason
+    print('Sending Packet........')
+    if (message == '!DISCONNECT'):
+        clientSocket.send(message.encode(FORMAT))
+    else:
+        clientSocket.send(message)
+
+def sendVideo():
+    vidcap = cv2.VideoCapture('video.mp4')
+    success,image = vidcap.read()
+    count = 0
+    while success:
+        send(encodeImage(image))   
+        success,image = vidcap.read()
+        count += 1
+
+while True:
+    clientSocket, clientAddress = server.accept()
+    # Send Image
+    # print(f'[CLIENT] connected to {clientAddress}')
+    # img = cv2.imread('panther.jpg')
+    # img_str = cv2.imencode('.jpeg', img)[1].tobytes()
+    # send(encodeImage(img))
+    sendVideo()
+    send(DISCONNECT_MESSAGE)
