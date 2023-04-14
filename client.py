2e12f8f7dd75f4dec080adfa8e59f15d1de44348
Seifeldin Sami <saifsamy2001@gmail.com>
2023-04-14 21:49:50 +0200

    Update client.py

--- client.py
+++ client.py
@@ -50,7 +50,7 @@ img = np.frombuffer(img, np.uint8)
 img = cv2.imdecode(img, 3)
 height, width, layers = img.shape
 size = (width,height)
-out = out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 24, size)
+out = out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 30, size)
 
 for i in range(len(images_arr)):
     img = images_arr[i]
